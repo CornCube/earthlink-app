7155fd785ddabc1e895c263a036d43767c58c413
CornCube <mmalla2002@gmail.com>
2023-09-26 22:11:18 -0500

    more map tweaking

--- app/src/main/java/com/example/earthlink/MainApp.kt
+++ app/src/main/java/com/example/earthlink/MainApp.kt
@@ -2,36 +2,35 @@ package com.example.earthlink
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.ui.viewinterop.AndroidView
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+
 
 @Composable
 fun Main() {
-    // add maps stuff here, it is the base. all rows and cols are over it.
     MapView(
         onLoad = { map ->
             map.setMultiTouchControls(true)
             map.controller.setZoom(9.0)
+            map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+            // uncomment below to remove the zoom buttons
+            // map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+
+            // get the current location, and set the map to that location
         }
     )
 
